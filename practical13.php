61c47777248e19a68a959464bb7e1e54a37ad2c4
Swechchha Singh <singhswechchha538@gmail.com>
2025-08-30 15:39:32 +0530

    Add files via upload

---
+++ practical13.php
@@ -0,0 +1,7 @@
+<?php
+$a = 33;
+$b = 44;
+$c = 67;
+$sum = $a+$b+$c;
+?>
+<?= "sum of the a,b and c:". $sum; 
---
+++ practical19.php
@@ -0,0 +1,16 @@
+<?php
+$arr[0]="swechchha";
+$arr[1]="Saumya";
+$arr[2]="Tripti";
+$arr[3]="Yashi";
+$arr[4]="Tanu";
+$arr[5]="Niyati";
+$arr[6]="Swati";
+$arr[7]="Ananya";
+$arr[8]="Purnima";
+$arr[9]="Shivanshi";
+echo"<h3>Indexed array: </h3>";
+for($i=0; $i<10; $i++){
+    echo "Indexed $i = ".$arr[$i]."<br>";
+}
+?>
